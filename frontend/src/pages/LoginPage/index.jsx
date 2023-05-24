d4208299a430ec5f095b4d273e80b73518259357
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-05-24 20:25:10 +0200

    Implement CSS module sign in form

--- frontend/src/pages/LoginPage/index.jsx
+++ frontend/src/pages/LoginPage/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuth } from '../../hooks/Auth';
+import styles from './index.module.scss';
 
 export default function LoginPage() {
   const { login } = useAuth();
@@ -10,23 +11,23 @@ export default function LoginPage() {
 
   return (
     <main className="main bg-dark">
-      <section className="sign-in-content">
-        <i className="fa fa-user-circle sign-in-icon"></i>
+      <section className={styles.form_section}>
+        <i className={`fa fa-user-circle ${styles.form_section__icon}`}></i>
         <h1>Sign In</h1>
         <form>
-          <div className="input-wrapper">
+          <div className={styles.form_section__input_wrapper}>
             <label htmlFor="username">Username</label>
             <input type="text" id="username" />
           </div>
-          <div className="input-wrapper">
+          <div className={styles.form_section__input_wrapper}>
             <label htmlFor="password">Password</label>
             <input type="password" id="password" />
           </div>
-          <div className="input-remember">
+          <div className={styles.form_section__input_remember}>
             <input type="checkbox" id="remember-me" />
             <label htmlFor="remember-me">Remember me</label>
           </div>
-          <button onClick={handleLogin} className="sign-in-button">
+          <button onClick={handleLogin} className={styles.form_section__button}>
             Sign In
           </button>
         </form>
