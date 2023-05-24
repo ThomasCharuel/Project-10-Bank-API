abf4f5d0d3a8fe83b6833dd5b2a9eb7e8c309e20
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-05-24 20:18:54 +0200

    CSS Module Landing Page

--- frontend/src/pages/LandingPage/index.jsx
+++ frontend/src/pages/LandingPage/index.jsx
@@ -3,6 +3,7 @@ import FeatureItem from '../../components/FeatureItem';
 import iconChat from '../../assets/img/icon-chat.png';
 import iconMoney from '../../assets/img/icon-money.png';
 import iconSecurity from '../../assets/img/icon-security.png';
+import styles from './index.module.scss';
 
 const features = [
   {
@@ -26,16 +27,16 @@ const features = [
 export default function LandingPage() {
   return (
     <main className="main">
-      <div className="hero">
-        <section className="hero-content">
+      <div className={styles.hero}>
+        <section className={styles.hero__content}>
           <h2 className="sr-only">Promoted Content</h2>
-          <p className="subtitle">No fees.</p>
-          <p className="subtitle">No minimum deposit.</p>
-          <p className="subtitle">High interest rates.</p>
-          <p className="text">Open a savings account with Argent Bank today!</p>
+          <p className={styles.hero__subtitle}>No fees.</p>
+          <p className={styles.hero__subtitle}>No minimum deposit.</p>
+          <p className={styles.hero__subtitle}>High interest rates.</p>
+          <p className={styles.hero__text}>Open a savings account with Argent Bank today!</p>
         </section>
       </div>
-      <section className="features">
+      <section className={styles.features}>
         <h2 className="sr-only">Features</h2>
         {features.map(({ image, title, paragraph }, i) => (
           <FeatureItem key={i} image={image} title={title} paragraph={paragraph} />
