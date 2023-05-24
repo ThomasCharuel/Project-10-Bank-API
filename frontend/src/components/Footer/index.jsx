3e44c0185933a7229de53172dbbf7b24424bc4e7
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-05-24 20:02:54 +0200

    Module CSS for footer

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './index.module.scss';
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <p className="footer-text">Copyright 2020 Argent Bank</p>
+    <footer className={styles.footer}>
+      <p className={styles.footer__text}>Copyright 2020 Argent Bank</p>
     </footer>
   );
 }
